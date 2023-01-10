48fd7cf505021fece6308eabebd7f16041b9ea66
JamieHa8794 <JamieHa8794@gmail.com>
2023-01-09 21:15:16 -0500

    able to display example chart

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
+import BarChart from './BarChart';
 
+const labels = ['2018', '2019', '2020', '2021']
+const data = [100, 150, 200, 250]
 
 class App extends Component{
     constructor(){
         super();
+        this.state = {
+            labels: ['2018', '2019', '2020', '2021'],
+            datasets: [{
+                label: "Users Gained", 
+                data: [100, 150, 200, 250]
+            }]
+        }
     }
+
+
     render(){
+        const {labels, datasets} = this.state
+    // this.setState({
+    //     labels: ['2018', '2019', '2020', '2021'],
+    //     datasets: [{
+    //         label: "Users Gained", 
+    //         data: [100, 150, 200, 250]
+    //     }]
+    // })
+    
         return(
-            <div id='main'>
+            <div className='main-box'>
                 <h1>
                     Hello World
                 </h1>
+                <BarChart chartData={{labels, datasets}}/>
             </div>
         )
     }
