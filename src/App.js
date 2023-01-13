4d77dc9e5ee783b723a59602b7222bfd15588672
JamieHa8794 <JamieHa8794@gmail.com>
2023-01-12 20:52:08 -0500

    refactoring test bar chat

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React, {Component, useState} from 'react';
+import React, {Component} from 'react';
 import BarChart from './BarChart';
 
-const labels = ['2018', '2019', '2020', '2021']
-const data = [100, 150, 200, 250]
+
 
 class App extends Component{
     constructor(){
         super();
         this.state = {
-            labels: ['2018', '2019', '2020', '2021'],
-            datasets: [{
-                label: "Users Gained", 
-                data: [100, 150, 200, 250]
-            }]
         }
     }
 
 
     render(){
-        const {labels, datasets} = this.state
-    // this.setState({
-    //     labels: ['2018', '2019', '2020', '2021'],
-    //     datasets: [{
-    //         label: "Users Gained", 
-    //         data: [100, 150, 200, 250]
-    //     }]
-    // })
-    
         return(
             <div className='main-box'>
                 <h1>
                     Hello World
                 </h1>
-                <BarChart chartData={{labels, datasets}}/>
+                <BarChart/>
             </div>
         )
     }
--- src/BarChart.js
+++ src/BarChart.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 import { Bar } from "react-chartjs-2";
 import {Chart as ChartJS} from 'chart.js/auto'
 
-function BarChart({chartData}){
-    return <Bar data={chartData} />
+
+class BarChart extends Component{
+    constructor(){
+        super();
+        this.state = {
+            labels: ['2018', '2019', '2020', '2021'],
+            datasets: [{
+                label: "Users Gained", 
+                data: [100, 150, 200, 250]
+            }]
+        }
+    }
+
+    render(){
+        const {labels, datasets} = this.state
+    
+        return(
+            <div className='main-box'>
+                <Bar data={{labels, datasets}}/>
+            </div>
+        )
+    }
 }
 
 export default BarChart;
